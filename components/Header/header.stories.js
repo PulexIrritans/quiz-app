a5fe3f1d9621b4a325d4c07bf9301264097f072d
Antje Exner <antjeexner@Antjes-Air.fritz.box>
2022-03-11 16:28:10 +0100

    add mainpage content and style

---
+++ components/Header/header.stories.js
@@ -0,0 +1,10 @@
+import "./header.css";
+
+export default {
+  title: "Components/Header",
+};
+
+export const Default = () => `<header class="quiz-header">
+<h1 class="quiz-header-h1">Quiz-App</h1>
+</header>`;
+
---
+++ components/Question Card/question-card.stories.js
@@ -0,0 +1,7 @@
+import "./question-card.css";
+
+export default {
+  title: "Components/question-card",
+};
+
+export const Default = () => 
