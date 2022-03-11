1b164b785c36b5583381f8f6c0555465a45709d5
Antje Exner <antjeexner@Antjes-Air.fritz.box>
2022-03-11 21:29:17 +0100

    Add components headlines, questioncards

---
+++ components/Headlines/headlines.stories.js
@@ -0,0 +1,8 @@
+import "./headlines.css";
+
+export default {
+  title: "Components/Headlines",
+};
+
+export const H1 = () => `<h1 class="quiz-header-h1">Quiz-App</h1>`;
+export const H2 = () => '<h2 class="h2">Question</h2>'
--- components/Question Card/question-card.stories.js
+++
@@ -1,7 +0,0 @@
-import "./question-card.css";
-
-export default {
-  title: "Components/question-card",
-};
-
-export const Default = () => 
