3d7593bdac21af4cfa7af010b2931255f010ae06
Antje Exner <antjeexner@Antjes-Air.fritz.box>
2022-03-11 23:41:44 +0100

    break footer

---
+++ components/TagList/taglist.stories.js
@@ -0,0 +1,14 @@
+import "./taglist.css";
+
+export default {
+  title: "Components/taglist",
+};
+
+export const Default = () => 
+
+`<ul class="tag-list">
+  <li class="tag">tagtag 1</li>
+  <li class="tag">tag 2</li>
+  <li class="tag">tagtagtag 3</li>
+  <li class="tag">tag 4</li>
+</ul>`
