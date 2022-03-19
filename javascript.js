8ff1d8860fe2170bdace07d8d2e28efb872e689e
Antje Exner <antjeexner@Antjes-Air.fritz.box>
2022-03-19 17:50:32 +0100

    fix toggle answer button

--- javascript.js
+++ javascript.js
@@ -3,31 +3,26 @@
 const questionCardButtonElements = document.querySelectorAll(
   'button[data-js="toggle-answer"]'
 );
-// const bookmarkElement = document.querySelectorAll('.question-card-svg path');
-const inputContainerElement = document.querySelectorAll('.input-container');
+const bookmarkElements = document.querySelectorAll('.question-card-svg path');
+const inputContainerElements = document.querySelectorAll('.input-container');
 const footerMenuElements = document.querySelectorAll('.quiz-footer-list-item');
-console.log(footerMenuElements)
-const questionCardElement = document.querySelectorAll('.question-card');
 
 
 // Functions
+/// Question card functions
 
-// function toggleAnswer(event) {
-//   event.target.nextElementSibling.classList.toggle('hidden');
-//   if (event.target.textContent === 'Show answer') {
-//     event.target.textContent = 'Hide answer';
-//   } else {
-//     event.target.textContent = 'Show answer';
-//   }
-// }
+function toggleAnswer(event) {
+  event.target.nextElementSibling.classList.toggle('hidden');
+  const buttonText = event.target.textContent.trim();
+  buttonText === 'Show answer' ? event.target.textContent = 'Hide answer' : event.target.textContent = 'Show answer';
+}
 
-// function toggleBookmark(event) {
-//   event.target.parentElement.classList.toggle('bookmarked');
-//   event.target.parentElement.parentElement.classList.toggle('bookmarked-question'); ---> Just tried here to give the whole card item a class when bookmarked
-// }
+function toggleBookmark(event) {
+  event.target.parentElement.classList.toggle('bookmarked');
+}
 
 
-// Function that adjust page content depending on clicked list menu item
+/// Function that adjust page content depending on clicked list menu item
 function changePageContent(event) {
     
     const allSectionElements = document.querySelectorAll('section')
@@ -37,12 +32,12 @@ function changePageContent(event) {
     const sectionProfileElement = document.querySelector('.profile');
     const pageHeaderElement = document.querySelector('.quiz-header-h1');
     
-    //Changes highlighting for active menu element
+    //// Changes highlighting for active menu element
     footerMenuElements.forEach(element => {
     element.classList.remove('item-active')})
     event.target.classList.add('item-active') 
 
-    //Changes visible section and header depending on clicked menu element
+    //// Changes visible section and header depending on clicked menu element
     allSectionElements.forEach(element => {
     element.classList.add('hidden')})
     const eventTargetAttribut = event.target.getAttribute("data-js");
@@ -69,8 +64,10 @@ function changePageContent(event) {
     }
 }
 
-// Event Listener and function for input count
-inputContainerElement.forEach(element => {
+// Event Listener
+
+/// Event Listener and function for input count
+inputContainerElements.forEach(element => {
   const inputFieldElement = element.querySelector('.create-form-input');
   const createFormCounterElement = element.querySelector(
     '.create-form-counter'
@@ -83,35 +80,21 @@ inputContainerElement.forEach(element => {
   });
 });
 
-// Event Listener Show-Hide Answer
-// questionCardButtonElements.forEach(element =>
-//   element.addEventListener('click', toggleAnswer)
-// );
+/// Event Listener Show-Hide Answer Button
+questionCardButtonElements.forEach(element =>
+  element.addEventListener('click', toggleAnswer)
+);
 
-// Event Listener Bookmark
-// bookmarkElement.forEach(element =>
-//   element.addEventListener('click', toggleBookmark)
-// );
+/// Event Listener Bookmark
+bookmarkElements.forEach(element =>
+  element.addEventListener('click', toggleBookmark)
+);
 
-// Event Listener for menu
+/// Event Listener for Menu items
 footerMenuElements.forEach(element => {
     element.addEventListener('click', changePageContent);
 });
 
-// Event Listener and function for question card button and bookmark
-questionCardElement.forEach(element => {
-    const buttonElement = element.querySelector('button')
-    buttonElement.addEventListener('click', () => {
-        element.querySelector('.question-card-answer').classList.toggle('hidden');
-        console.log(textContent)
-        if (event.target.textContent==='Show answer') {
-        target.textContent = 'Hide answer'
-        } else {target.textContent = 'Show answer'}
-        }
-    )    
-    const bookmarkElement = element.querySelector('path')
-    const bookmarkSvgElement = element.querySelector('.question-card-svg')
-    bookmarkElement.addEventListener('click', () => {
-    bookmarkSvgElement.classList.toggle('bookmarked')
-    })
-})
+
+
+
